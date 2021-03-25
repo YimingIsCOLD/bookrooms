beb3ce699f9d71239b4d242a376700adb6dd1717
YimingIsCOLD <yimingpeh@gmail.com>
2021-03-25 23:52:46 +0800

    Added React, typescript and prettier

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -3,13 +3,13 @@
 // a relevant structure within app/javascript and only use these pack files to reference
 // that code so it'll be compiled.
 
-import Rails from "@rails/ujs"
-import Turbolinks from "turbolinks"
-import * as ActiveStorage from "@rails/activestorage"
+import Rails from '@rails/ujs';
+import Turbolinks from 'turbolinks';
+import * as ActiveStorage from '@rails/activestorage';
 
 // Tailwind CSS.
-import "stylesheets/application.scss"
+import 'stylesheets/application.scss';
 
-Rails.start()
-Turbolinks.start()
-ActiveStorage.start()
+Rails.start();
+Turbolinks.start();
+ActiveStorage.start();
