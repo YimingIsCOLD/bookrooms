c4bfeb06e6eba7d766fbaed6b76f24c20f7e2c25
YimingIsCOLD <yimingpeh@gmail.com>
2021-03-30 02:07:24 +0800

    doc: added jsdoc for utils.ts

--- app/javascript/dashboard/utils.ts
+++ app/javascript/dashboard/utils.ts
@@ -1,10 +1,20 @@
 import { addMinutes, format, parse } from 'date-fns';
 import { Timing } from './types';
 
+/**
+ * Get the CSRF token from the HTML.
+ */
 export function getCSRFToken() {
   return document.querySelector<HTMLMetaElement>('meta[name="csrf-token"]')?.content || '';
 }
 
+/**
+ * Generates a list of timings with the given start time, interval and steps.
+ *
+ * @param startTime the starting time.
+ * @param minsInterval the time in minutes between each steps.
+ * @param steps the number entries to generate.
+ */
 export function generateTimings(startTime: string, minsInterval: number, steps: number) {
   const timings: Timing[] = [];
   let t = parse(startTime, 'HH:mm', new Date(2021, 0, 1));
